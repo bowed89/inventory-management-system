0994aa5164e892f840b44bd434f222c91e5e9739
bowed89 <jesus.lopez.averanga@gmail.com>
2026-07-01 22:02:18 -0400

    feat: create initial domain model and project structure

---
+++ backend/src/main/java/com/jesus/inventory/enums/TransactionStatus.java
@@ -0,0 +1,5 @@
+package com.jesus.inventory.enums;
+
+public enum TransactionStatus {
+    PENDING, PROCESSING, COMPLETED, CANCELED
+}
---
+++ backend/src/main/java/com/jesus/inventory/enums/TransactionType.java
@@ -0,0 +1,5 @@
+package com.jesus.inventory.enums;
+
+public enum TransactionType {
+    PURCHASE, SALE, RETURN_SUPPLIER
+}
