1e93d14dd670a0ee74688b58085471c116db5560
bowed89 <jesus.lopez.averanga@gmail.com>
2026-07-03 23:02:32 -0400

    feat: configure jwt authentication and spring security

---
+++ backend/src/main/java/com/jesus/inventory/security/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.jesus.inventory.security;
+
+import com.jesus.inventory.entity.User;
+import com.jesus.inventory.exceptions.NotFoundException;
+import com.jesus.inventory.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username)
+                .orElseThrow(() -> new NotFoundException("User Email Not Found"));
+
+        return AuthUser.builder()
+                .user(user)
+                .build();
+    }
+
+}
