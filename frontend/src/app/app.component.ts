be48b17b07cc4b0c49f05fc6e5e6fb2714f22699
bowed89 <jesus.lopez.averanga@gmail.com>
2026-07-17 22:48:44 -0400

    feat: add route guard logic and application layout

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { ApiService } from './service/api.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'frontend';
+  title = 'ims';
+
+  constructor(
+    private apiService: ApiService,
+    private router: Router,
+    private cdr: ChangeDetectorRef
+  ) {}
+
+  isAuth(): boolean {
+    return this.apiService.isAuthenticated();
+  }
+
+  isAdmin(): boolean {
+    return this.apiService.isAdmin();
+  }
+
+  logout(): void {
+    this.apiService.logout();
+    this.router.navigate(['/login']);
+    this.cdr.detectChanges(); // Trigger change detection to update the view
+  }
+
 }
