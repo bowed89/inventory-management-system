c5f3fd4d669ee85669b1c254536ba73bb0f6c748
bowed89 <jesus.lopez.averanga@gmail.com>
2026-07-31 11:55:54 -0400

    feat: implement product and pagination components

--- frontend/src/app/product/product.component.ts
+++ frontend/src/app/product/product.component.ts
@@ -1,12 +1,79 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { PaginationComponent } from '../pagination/pagination.component';
+import { ApiService } from '../service/api.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, PaginationComponent],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit {
+  constructor(
+    private apiService: ApiService,
+    private router: Router
+  ) { }
 
+  products: any[] = [];
+  message: string = '';
+  currentPage: number = 1;
+  totalPages: number = 0;
+  itemsPerPage: number = 10;
+
+  ngOnInit() {
+    this.fetchProducts();
+  }
+
+  fetchProducts(): void {
+    this.apiService.getAllProducts().subscribe({
+      next: (res: any) => {
+        const products = res.products || [];
+        this.totalPages = Math.ceil(products.length / this.itemsPerPage);
+        this.products = products.slice((this.currentPage - 1) * this.itemsPerPage, this.currentPage * this.itemsPerPage);
+      },
+      error: (error: any) => {
+        this.showMessage(error?.error?.message || error?.message || 'An error occurred while showing products');
+      }
+    })
+
+  }
+
+  handleProductDelete(productId: string): void {
+    if (window.confirm("Are you sure you want to delete this product?")) {
+      this.apiService.deleteProduct(productId).subscribe({
+        next: (response: any) => {
+          if (response.status === 200) {
+            this.showMessage("Product deleted successfully");
+            this.fetchProducts();
+          }
+        },
+        error: (error: any) => {
+          this.showMessage(error?.error?.message || error?.message || 'An error occurred while deleting product' + error);
+        }
+      })
+    }
+  }
+
+  onPageChange(page: number): void {
+    this.currentPage = page;
+    this.fetchProducts();
+  }
+
+  navigateToAddProductPage(): void {
+    this.router.navigate(['/add-product']);
+  }
+
+  navigateToEditProductPage(productId: string): void {
+    this.router.navigate([`/edit-product/${productId}`]);
+  }
+
+  showMessage(msg: string) {
+    this.message = msg;
+    setTimeout(() => {
+      this.message = '';
+    }, 3000);
+  }
 }
