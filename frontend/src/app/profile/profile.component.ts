8357e896fff176c0f16940124306308f24df0b69
bowed89 <jesus.lopez.averanga@gmail.com>
2026-08-02 19:23:38 -0400

    feat: implement dashboard, profile and sell components

--- frontend/src/app/profile/profile.component.ts
+++ frontend/src/app/profile/profile.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../service/api.service';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  constructor(
+    private apiService: ApiService
+  ) { }
+
+  user: any = null;
+  message: string = '';
+
+  ngOnInit(): void {
+    this.fetchUserInfo();
+  }
+
+  fetchUserInfo(): void {
+    this.apiService.getLoggedInUserInfo().subscribe({
+      next: (res: any) => {
+        this.user = res;
+      },
+      error: (error: any) => {
+        this.showMessage(error?.error?.message || error?.message || 'An error occurred while getting user info' + error);
+      }
+    });
+  }
+
+  showMessage(msg: string) {
+    this.message = msg;
+    setTimeout(() => {
+      this.message = '';
+    }, 3000);
+  }
 
 }
